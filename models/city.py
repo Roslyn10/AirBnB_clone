f5a2406d463d08746838f7526652a0b0d89d9ac9
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-20 14:37:25 +0200

    Adding the code for city.py

--- models/city.py
+++ models/city.py
@@ -3,3 +3,14 @@
 
 
 class City(BaseModel):
+    """
+    A class representing a city.
+
+    Attributes:
+        state_id (str): The id of a state.
+        name (str): The name of the city.
+
+    """
+
+    state_id = ""
+    name = ""
