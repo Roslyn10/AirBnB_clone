ee9b9886915ac097cbd6a80b8c5a6a2adb095db7
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-20 14:44:34 +0200

    Adding the code for review.py

--- models/review.py
+++ models/review.py
@@ -3,3 +3,16 @@
 
 
 class Review(BaseModel):
+    """
+    A class representing a review. 
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The review text given by the user.
+
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
