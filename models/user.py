25c0e23096b90b422abab19cd99123eb2d949996
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-20 14:27:47 +0200

    Adding the code for user class

--- models/user.py
+++ models/user.py
@@ -4,4 +4,17 @@
 
 class User(BaseModel):
     """
-    
+    A class representing a user.
+
+    Attributes:
+        email (str): The user's email address. 
+        password (str): The user's password.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name. 
+
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_namr = ""
