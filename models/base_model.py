38a2afdec4f94a9e0e7aaa88e3c0fb6d0f9929cd
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-22 15:13:49 +0200

    Adding basemodel codes  for id and __str__

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Base Model for the AirBnB clone"""
+
+import uuid
+from datetime import dateime
+
+def __init__(self, *args, **kwargs):
+    """Initializes the ...."""
+
+
+def __str__(self):
