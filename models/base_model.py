f43648439b3c9607ad51ca2a2fbd221afffa3e98
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-22 15:35:31 +0200

    Adding the other code and fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,14 @@
 import uuid
 from datetime import dateime
 
-def __init__(self, *args, **kwargs):
-    """Initializes the ...."""
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.name__, self.id, slef.__dict__)
 
-def __str__(self):
+    def save(self):
+        self.updated_at = datetime.now()
