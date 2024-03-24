050f017087d4e970340c406df8aa3fc327512344
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-25 00:31:24 +0200

    Adding code for console.py 6. entry point of the interpreter

--- console.py
+++ console.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 """Implementing the HBHBC console/ Entry point for the console"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Entry point of the command interpreter"""
+
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quites the console"""
+        return True
+
+    def do_end_of_file(self,arg):
+        """Indicates the end of the file"""
+        return True
+
+    def do_empty_line(self):
+        """Doesnt do anything"""
+        pass
+
+if __name__ == '__main__':
+HBNBCommand().cmdloop()
