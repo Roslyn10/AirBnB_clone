480e4d74b8fb7fc5c1b2128e58c39fef63ca401f
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-24 23:14:07 +0200

    Adding test files for amenity and base_model

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines unittests for amenity.py"""
+
+import os
+import datetime
+import unittest
+import models.amenity import Amenity
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines unittests for base model.py"""
+
+import os
+import unittest
+import datetime from datetime
+import models.base_model import BaseModel
