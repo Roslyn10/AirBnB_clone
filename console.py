ab1abb459aa28992714c3e61093eb91f26bc57fc
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-25 00:33:55 +0200

    Fixing the space around the prompt and indentation in the last line

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Entry point of the command interpreter"""
 
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """Quites the console"""
@@ -21,4 +21,4 @@ class HBNBCommand(cmd.Cmd):
         pass
 
 if __name__ == '__main__':
-HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
