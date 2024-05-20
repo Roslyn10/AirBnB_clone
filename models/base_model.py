bed0eed306fcec027cbb656ab921ad92ed4365b8
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-20 12:16:39 +0200

    Updating the BaseModel at __init__ regarding kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -21,11 +21,18 @@ class BaseModel:
 
         Arguments:
             *args: Length of the argument list.
-            **kwargs: Keyqword arguments.
+            **kwargs: Keyword arguments.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
