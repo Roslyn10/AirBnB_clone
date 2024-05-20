4b06d8e4bce72c0747432a77b9ed3aba89f57fd5
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-20 12:44:54 +0200

    Updating the __init__.py for the FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 """Defines a class FileStorage"""
+import json
 
 
 class FileStorage:
+    """A class that serializes instannces to a JSON file and deserializes JSOn file to instances"""
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+
+    
