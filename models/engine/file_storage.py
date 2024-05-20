1b54bee4084ebf518b5c3a985e949710ee314ce5
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-20 12:56:25 +0200

    Adding the code for FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,8 @@ import json
 
 
 class FileStorage:
-    """A class that serializes instannces to a JSON file and deserializes JSOn file to instances"""
+    """A class that serializes instances to a JSON file
+    and deserializes JSON file to instances"""
 
     __file_path = 'file.json'
     __objects = {}
@@ -15,13 +16,25 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        
+        key = f"{obj.__class__.name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-
+        obj_dict =
+        {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.--file_path, 'w') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-
-    
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    cls_name = value['__class__']
+                    if cls_name == 'BaseModel':
+                        obj = BaseModel(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
