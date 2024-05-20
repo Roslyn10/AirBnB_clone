59a87a3fb575ab9e9b16ef5119fe662af3dff527
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-20 11:25:45 +0200

    Adding documments to the code

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,31 @@ from datetime import datetime
 
 
 class BaseModel:
+    """
+    A base class for all models in the AirBnB clone projet.
+
+    Attributes:
+        id (str): Unique identidier for each instance.
+        created_at (datetime): The date and time when the instances created.
+        updated_at (datetime): Date and time when the instance is last updated
+        """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel
+
+        Arguments:
+            *args: Length of the argument list.
+            **kwargs: Keyqword arguments.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """
+        Returns a string representation of the BaseModel instances
+        """
         return "[{}] ({}) {}".format(
                 self.__class__.__name__,
                 self.id,
@@ -19,9 +38,16 @@ class BaseModel:
         )
 
     def save(self):
+        """
+        Updates updated_at with the current date and time
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary representation of the instance
+
+        """
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
